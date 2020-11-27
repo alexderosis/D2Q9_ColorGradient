eaada1fcf763dc37e7f388a24c367872a3a5a497
Alessandro De Rosis <derosis.alessandro@icloud.com>
2020-11-27 14:00:08 +0100

    wip

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ using namespace std;
 ///----------------------------------------------------------------------------------------------------------------------------------
 const bool plot_vtk = true;
 const int n_phase = 2;
-const int nx = 100, ny = 2*nx, np = 9;
-const double NX = (double)nx-1, gravity = 0.05*0.05/NX, rho0_b = 1., At = 0.9, rho0_r = -rho0_b*(At+1)/(At-1), Reynolds = 30000., nu = sqrt(NX*gravity)*NX/Reynolds, T = sqrt(NX/gravity/At);
+const int nx = 1000, ny = 2*nx, np = 9;
+const double NX = (double)nx-1, gravity = 0.05*0.05/NX, rho0_b = 1., At = 0.7, rho0_r = -rho0_b*(At+1)/(At-1), Reynolds = 3000000., nu = sqrt(NX*gravity)*NX/Reynolds, T = sqrt(NX/gravity/At);
 const double cs2 = 1./3., beta = 0.7, alpha_b = 4./9., alpha_r = 1.-(1.-alpha_b)*rho0_b/rho0_r;
 vector<const int> cx = {0, 1, 0, -1, 0, 1, -1, -1, 1},
 									cy = {0, 0, 1, 0, -1, 1, 1, -1, -1},
@@ -22,20 +22,24 @@ vector<const int> cx = {0, 1, 0, -1, 0, 1, -1, -1, 1},
 vector<const double> wf = {4./9., 1./9., 1./9., 1./9., 1./9., 1./36., 1./36., 1./36., 1./36.};
 vector<const double> B = {-4/27., 2/27., 2/27., 2/27., 2/27., 5/108., 5/108., 5/108., 5/108.};
 vector<const double> alphaK = {alpha_b, alpha_r}, ni = {nu, nu};
-const int nsteps = (int)(10*T+1), n_out = (int)(T/20);
-vector<double> f(nx*ny*np,0.), f_old(nx*ny*np,0.), rhoK(nx*ny*n_phase,0.), rhoK_old(nx*ny*n_phase,0.), rho(nx*ny,0.), u(nx*ny,0.), v(nx*ny,0.);
+const int nsteps = (int)(10*T+1), n_out = (int)(T/100);
+vector<double> f(nx*ny*np,0.), f_old(nx*ny*np,0.), rhoK(nx*ny*n_phase,0.), rhoK_old(nx*ny*n_phase,0.), rho(nx*ny,0.), u(nx*ny,0.), v(nx*ny,0.), rho_old(nx*ny,0.);
 vector<double> gradx_rhoK(nx*ny*n_phase,0.), grady_rhoK(nx*ny*n_phase,0.);
 vector<double> gradx_rho(nx*ny,0.), grady_rho(nx*ny,0.);
-double U, V, W, ftemp, A, C;
+double U, V, W, ftemp, A, C, R;
 double colorGradX, colorGradY, colorGradNorm, tempx, tempy, norm_c, tmp, tmp3, phi;
-double alpha, totalMass;
-vector<double> streamed(nx*ny,0.), kTotalMass(nx*ny,0.);
+double alpha, totalMass, streamed;
+vector<double> kTotalMass(nx*ny,0.);
+const double surfaceTension = 1E-5;
+double A_, R1, nu_eff, omega_eff, GX, GY;
+int check;
 ///-CMS
+vector<double> temp_pop(np,0.);
 double k0, k1, k2, k3, k4, k5, k6, k7, k8;
 double r0, r1, r2, r3, r4, r5, r6, r7, r8;
 double CX, CY, U2, V2, UV, U3, V3;
-double max_velocity;
-int id, idn;
+double value;
+int id, idn, newx, newy, id1, id2;
 double FX, FY;
 ///----------------------------------------------------------------------------------------------------------------------------------
 ///----------------------------------------------------------------------------------------------------------------------------------
@@ -147,17 +151,15 @@ void initial_state()
 void compute_gradient_rho(int x, int y)
 {
   id = x*ny+y;
-	int newx, newy, id1, id2;
-  double value;
   for(int k=0; k<n_phase; k++)
   {
   	gradx_rhoK[id*n_phase+k] = 0.;
     grady_rhoK[id*n_phase+k] = 0.;
-		if(k==0)
+		/*if(k==0)
 		{
 			gradx_rho[id] = 0.;
 			grady_rho[id] = 0.;
-		}
+		}*/
 		if(y>0 && y<ny-1)
 	    for(int n=1; n<np; n++)
 		  {
@@ -168,42 +170,42 @@ void compute_gradient_rho(int x, int y)
 			  if(y==0 || y==ny-1)
 				 newy = (newy+ny)%ny;
         idn = newx*ny+newy;
-	  		value = rhoK[idn*n_phase+k]/rho[idn];
+	  		value = rhoK[idn*n_phase+k]/rho_old[idn];
 			  gradx_rhoK[id*n_phase+k] += 3.*wf[n]*cx[n]*value;
 			  grady_rhoK[id*n_phase+k] += 3.*wf[n]*cy[n]*value;
-				if(k==0)
+				/*if(k==0)
 				{
 					gradx_rho[id] += 3.*wf[n]*cx[n]*rho[idn];
 					grady_rho[id] += 3.*wf[n]*cy[n]*rho[idn];
-				}
+				}*/
 			}
 		if(y==0) // SOUTH wall
 		{
       id1 = ((x+1+nx)%nx)*ny+y;
       id2 = ((x-1+nx)%nx)*ny+y;
-			gradx_rhoK[id*n_phase+k] = 0.5*(rhoK[id1*n_phase+k]/rho[id1] - rhoK[id2*n_phase+k]/rho[id2]);
+			gradx_rhoK[id*n_phase+k] = 0.5*(rhoK[id1*n_phase+k]/rho_old[id1] - rhoK[id2*n_phase+k]/rho_old[id2]);
 			if(x==0)
-				gradx_rhoK[id*n_phase+k] = -rhoK[id*n_phase+k]/rho[id] + rhoK[id1*n_phase+k]/rho[id1];
+				gradx_rhoK[id*n_phase+k] = -rhoK[id*n_phase+k]/rho_old[id] + rhoK[id1*n_phase+k]/rho_old[id1];
 			if(x==nx-1)
-				gradx_rhoK[id*n_phase+k] = rhoK[id*n_phase+k]/rho[id] - rhoK[id2*n_phase+k]/rho[id2];
+				gradx_rhoK[id*n_phase+k] = rhoK[id*n_phase+k]/rho_old[id] - rhoK[id2*n_phase+k]/rho_old[id2];
 
       id1 = x*ny+(y+1);
 			id2 = x*ny+(y+2);
-			grady_rhoK[id*n_phase+k] = -3./2*rhoK[id*n_phase+k]/rho[id] + 2.*rhoK[id1*n_phase+k]/rho[id1] - 1./2.*rhoK[id2*n_phase+k]/rho[id2];
+			grady_rhoK[id*n_phase+k] = -3./2*rhoK[id*n_phase+k]/rho_old[id] + 2.*rhoK[id1*n_phase+k]/rho_old[id1] - 1./2.*rhoK[id2*n_phase+k]/rho_old[id2];
 		}
 		if(y==ny-1) // NORTH wall
 		{
       id1 = ((x+1+nx)%nx)*ny+y;
       id2 = ((x-1+nx)%nx)*ny+y;
-			gradx_rhoK[id*n_phase+k] = 0.5*(rhoK[id1*n_phase+k]/rho[id1] - rhoK[id2*n_phase+k]/rho[id2]);
+			gradx_rhoK[id*n_phase+k] = 0.5*(rhoK[id1*n_phase+k]/rho_old[id1] - rhoK[id2*n_phase+k]/rho_old[id2]);
 			if(x==0)
-				gradx_rhoK[id*n_phase+k] = -rhoK[id*n_phase+k]/rho[id] + rhoK[id1*n_phase+k]/rho[id1];
+				gradx_rhoK[id*n_phase+k] = -rhoK[id*n_phase+k]/rho_old[id] + rhoK[id1*n_phase+k]/rho_old[id1];
 			if(x==nx-1)
-				gradx_rhoK[id*n_phase+k] = rhoK[id*n_phase+k]/rho[id] - rhoK[id2*n_phase+k]/rho[id2];
+				gradx_rhoK[id*n_phase+k] = rhoK[id*n_phase+k]/rho_old[id] - rhoK[id2*n_phase+k]/rho_old[id2];
 
       id1 = x*ny+(y-1);
 			id2 = x*ny+(y-2);
-			grady_rhoK[id*n_phase+k] = 3./2*rhoK[id*n_phase+k]/rho[id] - 2.*rhoK[id1*n_phase+k]/rho[id1] + 1./2.*rhoK[id2*n_phase+k]/rho[id2];
+			grady_rhoK[id*n_phase+k] = 3./2*rhoK[id*n_phase+k]/rho_old[id] - 2.*rhoK[id1*n_phase+k]/rho_old[id1] + 1./2.*rhoK[id2*n_phase+k]/rho_old[id2];
 		}
 	}
 }
@@ -211,11 +213,10 @@ void compute_gradient_rho(int x, int y)
 void perturbation(int x, int y, double omega)
 {
   id = x*ny+y;
+	A_ = 9*surfaceTension*omega*0.25;
+	R1 = 1./rho[id];
   for(int k=0; k<n_phase; k++)
   {
-		double surfaceTension = 1E-5;
-    double A_ = 9*surfaceTension*omega*0.25;
-    double R1 = 1./rho[id];
     for(int l=0; l<n_phase; l++)
     	if(k!=l)
     	{
@@ -238,45 +239,43 @@ void perturbation(int x, int y, double omega)
 ///----------------------------------------------------------------------------------------------------------------------------------
 void recoloring_and_streaming()
 {
-	int newx, newy;
 	f_old = f;
   rhoK_old = rhoK;
   std::fill(f.begin(), f.end(), 0.);
-  totalMass = 0.;
-  double R;
 	for(int k=0; k<n_phase; k++)
   {
-		std::fill(kTotalMass.begin(), kTotalMass.end(), 0);
-    for(int n=0; n<np; n++)
-    {
-      for(int x=0; x<nx; x++)
-		    for(int y=0; y<ny; y++)
-				{
-          id = x*ny+y;
-					R = rho[id];
-  	      tempx = tempy = 0.;
-  	      for(int l=0; l<n_phase; l++)
-  	      	if(k!=l)
-  	        {
-  	        	colorGradX = gradx_rhoK[id*n_phase+k]*rhoK_old[id*n_phase+l]-
+		std::fill(kTotalMass.begin(), kTotalMass.end(), 0.);
+    for(int x=0; x<nx; x++)
+			for(int y=0; y<ny; y++)
+			{
+        id = x*ny+y;
+				R = rho[id];
+				R1 = 1./R;
+  	    tempx = tempy = 0.;
+  	    for(int l=0; l<n_phase; l++)
+  	    	if(k!=l)
+  	      {
+  	      	colorGradX = gradx_rhoK[id*n_phase+k]*rhoK_old[id*n_phase+l]-
               	     		     gradx_rhoK[id*n_phase+l]*rhoK_old[id*n_phase+k];
-    					colorGradY = grady_rhoK[id*n_phase+k]*rhoK_old[id*n_phase+l]-
+    				colorGradY = grady_rhoK[id*n_phase+k]*rhoK_old[id*n_phase+l]-
           		          		 grady_rhoK[id*n_phase+l]*rhoK_old[id*n_phase+k];
-    					colorGradNorm = max(sqrt(colorGradX*colorGradX+
+    				colorGradNorm = max(sqrt(colorGradX*colorGradX+
   	          	  						         colorGradY*colorGradY),1e-12);
-  	          tempx += beta*colorGradX/colorGradNorm*rhoK_old[id*n_phase+l];
-  	          tempy += beta*colorGradY/colorGradNorm*rhoK_old[id*n_phase+l];
-						}
+  	        tempx += beta*colorGradX/colorGradNorm*rhoK_old[id*n_phase+l];
+  	        tempy += beta*colorGradY/colorGradNorm*rhoK_old[id*n_phase+l];
+					}
+				alpha = 0.;
+				for(int kk=0; kk<n_phase; kk++)
+					alpha += rhoK_old[id*n_phase+kk]*alphaK[kk];
+				alpha *= R1;
+				for(int n=0; n<np; n++)
+				{
   	      norm_c = sqrt(cx[n]*cx[n]+cy[n]*cy[n]);
   	      if(n==0)
   	      	norm_c = 0.;
   	      else
   	      	norm_c = 1./norm_c;
   	      tmp = tempx*cx[n]*norm_c+tempy*cy[n]*norm_c;
-  	      alpha = 0.;
-					for(int kk=0; kk<n_phase; kk++)
-				  	alpha += rhoK_old[id*n_phase+kk]*alphaK[kk];
-				  alpha /= R;
           if(n==0)
   	        phi = alpha;
   	      else if(n<5)
@@ -291,31 +290,20 @@ void recoloring_and_streaming()
   	      if(y==0 || y==ny-1)
   	        newy = (newy+ny)%ny;
   	      idn = newx*ny+newy;
-        	streamed[idn] = (f_old[id*np+n]+tmp)*rhoK_old[id*n_phase+k]/R;
+        	streamed = (f_old[id*np+n]+tmp)*rhoK_old[id*n_phase+k]*R1;
+					f[idn*np+n] += streamed;
+					kTotalMass[idn] += streamed;
+					rhoK[idn*n_phase+k] = kTotalMass[idn];
 				}
-      for(int x=0; x<nx; x++)
-		    for(int y=0; y<ny; y++)
-			  {
-          id = x*ny+y;
-  	      f[id*np+n] += streamed[id];
-  	      kTotalMass[id] += streamed[id];
-		  	}
-    }
-    for(int x=0; x<nx; x++)
-		  for(int y=0; y<ny; y++)
-			{
-        id = x*ny+y;
-			  rhoK[id*n_phase+k] = kTotalMass[id];
-			  totalMass += rhoK[id*n_phase+k];
 			}
   }
 }
 ///----------------------------------------------------------------------------------------------------------------------------------
 int algorithm_CMS()
 {
-  double R, nu, omega_eff, GX, GY;
-	int check = 0;
-	max_velocity = 0.;
+	rho_old = rho;
+	check = 0;
+	totalMass = 0.;
 	/// 1. Compute color-blind macroscopic variables
 	for(int x=0; x<nx; x++)
 		for(int y=0; y<ny; y++)
@@ -325,44 +313,31 @@ int algorithm_CMS()
 			FX = FY = 0.;
 	    for(int n=0; n<np; n++)
 	    {
-	    	ftemp = f[id*np+n];
+	    	ftemp = temp_pop[n] = f[id*np+n];
 	      R += ftemp;
 	      U += ftemp*cx[n];
 	      V += ftemp*cy[n];
 	    }
-	    // if(y == 0 || y == ny-1)
-	    // 	FY = 0.;
-	    // else
-		    FY = -(R - 0.5*(rho0_r+rho0_b))*gravity;
-	    U /= R;
-	    V = (V + 0.5*FY)/R;
+			R1 = 1./R;
+		  FY = -(R - 0.5*(rho0_r+rho0_b))*gravity;
+	    U *= R1;
+	    V = (V + 0.5*FY)*R1;
 	    rho[id] = R;
 	    u[id] = U;
 			v[id] = V;
-			if(sqrt(U*U+V*V)>max_velocity)
-				max_velocity = sqrt(U*U+V*V);
-		}
-	/// 2. Collide, compute gradients, perturb, recolor and stream
-	for(int x=0; x<nx; x++)
-		for(int y=0; y<ny; y++)
-		{
+			totalMass += R;
 			compute_gradient_rho(x, y);
-      id = x*ny+y;
 			GX = gradx_rho[id];
 			GY = grady_rho[id];
-			/// 2.1 collide the color-blind populations
-			R = rho[id];
-			U = u[id];
-			V = v[id];
-			alpha = nu = 0.;
+			alpha = nu_eff = 0.;
 			for(int k=0; k<n_phase; k++)
 			{
 				 alpha += rhoK[id*n_phase+k]*alphaK[k];
-				 nu += rhoK[id*n_phase+k]*ni[k];
+				 nu_eff += rhoK[id*n_phase+k]*ni[k];
 			}
-			alpha /= R;
-			nu /= R;
-			omega_eff = 1./(3.*nu+0.5);
+			alpha *= R1;
+			nu_eff *= R1;
+			omega_eff = 1./(3.*nu_eff+0.5);
 			if(fabs(U)>100.)
         check = 1;
       ///compute moments
@@ -371,31 +346,19 @@ int algorithm_CMS()
 			UV = U*V;
 			U3 = U2*U;
 			V3 = V2*V;
-			/*k4 = k5 = 0;
-		  for(int n=0; n<np; n++)
-      {
-        CX = (double)cx[n];
-        CY = (double)cy[n];
-        ftemp = f[id*np+n];
-        k4 += ftemp*(CX*CX-CY*CY);
-				k5 += ftemp*CX*CY;
-			}*/
-			r4 = f[id*np+1]-f[id*np+2]+f[id*np+3]-f[id*np+4];
-			r5 = f[id*np+5]-f[id*np+6]+f[id*np+7]-f[id*np+8];
+			r4 = temp_pop[1]-temp_pop[2]+temp_pop[3]-temp_pop[4];
+			r5 = temp_pop[5]-temp_pop[6]+temp_pop[7]-temp_pop[8];
 			k4 = r4-R*(U2-V2);
 			k5 = r5-R*UV;
-      FY = -(R - 0.5*(rho0_r+rho0_b))*gravity;
-			// if(y==0 || y==ny-1)
-			// 	FY = 0.;
 			k0 = R;
-      k1 = FX/2.;
-      k2 = FY/2.;
-      k3 = -(6*R*(alpha - 1.))/5.;
+      k1 = 0.5*FX*R1;
+      k2 = 0.5*FY*R1;
+      k3 = -6.*R*(alpha-1.)/5.;
       k4 *= 1.-omega_eff;
       k5 *= 1.-omega_eff;
-      k6 = 0.5*cs2*FY/R + R*V*(9.*alpha-4.)/15.;
-      k7 = 0.5*cs2*FX/R + R*U*(9.*alpha-4.)/15.;
-			k8 = -R*(3*alpha + 9*U2*alpha + 9*V2*alpha - 4*U2 - 4*V2 - 3.)/15.;
+      k6 = 0.5*cs2*FY*R1 + R*V*(9.*alpha-4.)/15.;
+      k7 = 0.5*cs2*FX*R1 + R*U*(9.*alpha-4.)/15.;
+			k8 = -R*(3.*(alpha-1.)+(U2+V2)*(9.*alpha-4.))/15.;
 
 			/*k0 += 3.*(U*GY+V*GX)*nu;
 			k1 += -3.*(GY*U2+V*GX*U)*nu;
@@ -444,12 +407,6 @@ void boundary()
 	for(int i=0; i<nx; i++)
 		for(int n=0; n<np; n++)
 		{
-			if(n==0)
-				phi = alpha;
-			else if(n<5)
-				phi = (1.-alpha)/5.;
-			else
-				 phi = (1.-alpha)/20.;
       if(cy[n]>0)
 			{
   			id = i*ny+0;
@@ -481,7 +438,7 @@ int main(int argc, char *argv[])
 			write_fluid_vtk(i);
     if(i%1==0)
       printf("Iteration %d of %d. TotMass=%e\n", i, nsteps, totalMass);
-    fprintf(data_output,"%d    %e\n", i, max_velocity);
+    fprintf(data_output,"%d    %e\n", i, totalMass);
   }
   labelA:
   fclose(data_output);
